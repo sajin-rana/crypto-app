dc2f7510e02fe219c967e279ded5583a0f1a9cd5
Sajin Rana <sajinrana@Sajins-MacBook-Pro.local>
2024-07-06 22:21:12 +0800

    up down arrow feature in dropdown

--- src/app/components/ui/PortfolioDropDown/PortfolioDropDown.tsx
+++ src/app/components/ui/PortfolioDropDown/PortfolioDropDown.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useDispatch } from "react-redux";
@@ -6,6 +6,7 @@ import { dropDownColor } from "@/app/utils/utils";
 import { setCoinOne } from "@/lib/features/cryptoSlice";
 import DropDownLoading from "../DropDownLoading/DropDownLoading";
 import { useGetSearchQueryDataQuery } from "@/lib/features/cryptoApi";
+import { useDropDownUpDownKeypress } from "@/app/customHook/CustomHook";
 const inter = Inter({ weight: "400", subsets: ["latin"] });
 
 const PortfolioDropDown = ({
@@ -17,6 +18,7 @@ const PortfolioDropDown = ({
   searchCoinInput: string;
   setSearchCoinInput: any;
 }) => {
+  const [index, setIndex] = useState<any>(null);
   const { data, isLoading } = useGetSearchQueryDataQuery(searchCoinInput);
   const { hoverColor, isDarkColor, textColor } = dropDownColor(isDark);
   const dispatch = useDispatch();
@@ -25,6 +27,11 @@ const PortfolioDropDown = ({
     dispatch(setCoinOne(id));
     setSearchCoinInput("");
   }
+
+  useDropDownUpDownKeypress(index, setIndex, data?.coins?.length, function () {
+    const coinId = data?.coins?.[index].id;
+    handleClick(coinId);
+  });
 
   return (
     <ul
@@ -35,11 +42,13 @@ const PortfolioDropDown = ({
       <li className="flex justify-center items-center mt-[10px]">
         {isLoading && <DropDownLoading />}
       </li>
-      {data?.coins.map((item: any) => (
+      {data?.coins.map((item: any, i: number) => (
         <li
           key={item.id}
           onClick={() => handleClick(item.id)}
-          className={`cursor-pointer ${inter.className} text-[12px] sm:text-[14px]  py-[6px] px-[12px] sm:py-[8px] sm:px-[16px] flex gap-[10px] sm:gap-[20px] ${hoverColor} ${textColor}`}
+          className={`cursor-pointer ${inter.className}  ${
+            index === i ? hoverColor.slice(6) : ""
+          } text-[12px] sm:text-[14px]  py-[6px] px-[12px] sm:py-[8px] sm:px-[16px] flex gap-[10px] sm:gap-[20px] ${hoverColor} ${textColor}`}
         >
           <div className="h-[24px] w-[24px]">
             <Image src={item.large} alt={item.name} height={24} width={24} />
